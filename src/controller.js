f605e7e6d6d1683f7b9e22f6ff30edd691f61c24
a.espolov <a.espolov@gmail.com>
2019-03-12 00:02:03 +0300

    next version

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,8 @@
 import App from 'cerebral';
 import Devtools from 'cerebral/devtools';
-import page from 'page';
-import qs from 'query-string';
 import defaultState from './cerebral/defaultState';
 import sequences from './cerebral/sequences';
-import Root from "./components/pages/Root";
-import NotFound from "./components/pages/NotFound";
+import router from "./cerebral/modules/router";
 
 let app;
 
@@ -17,13 +14,10 @@ export default () => {
     const main = {
         state,
         modules: {
-            // ...yourModulesHere
+            router,
         },
         sequences: {
-            routeToRoot: route('/', sequences.routeToRoot),
-            routeToRegistry: route('/history/registry/', sequences.routeToRegistry),
-            // routeToUser: route('/history/user/:id', routeToUser),
-            routeToNotFound: route('*', NotFound),
+            ...sequences
         },
         catch: [
             [
@@ -45,28 +39,6 @@ export default () => {
                 }),
         throwToConsole: false
     });
-    app.on('initialized', () => {
-        console.log('ggg initialized');
-        page.start();
-    });
-
-    function route(url, sequence) {
-        page(url, ({path, params, querystring}) => {
-            const query = qs.parse(querystring)
-
-            app.runSequence(path, sequence, {params, query})
-        });
-
-        return ({props}) => {
-            const urlWithReplacedParams = Object.keys(props.params || {}).reduce((currentUrl, param) => {
-                return currentUrl.replace(`:${param}`, props.params[param])
-            }, url);
-            const query = props.query ? '?' + qs.stringify(props.query) : '';
-
-            page.show(urlWithReplacedParams + query);
-        };
-    };
-
 
     if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
         window.__APP_CONTROLLER__ = app;
