9f46fb459c427de699c89cb2caeb2cfc92d26d77
a.espolov <a.espolov@gmail.com>
2019-03-12 23:46:01 +0300

    next version

--- src/components/pages/history/utils/filterList.js
+++
@@ -1,1 +0,0 @@
-const filterList = [  {    id: 1,    text: 'Добавлен собственник',    value: 'AddNewOwner'  },  {    id: 2,    text: 'Был импорт реестра',    value: 'Import'  },  {    id: 3,    text: 'Удален собственник',    value: 'DeleteOwner'  }];export default filterList;
