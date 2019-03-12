b32c1e0d8d8641c81fc7676bdcbcd3712d6c5445
a.espolov <a.espolov@gmail.com>
2019-03-12 23:04:39 +0300

    next version

--- src/cerebral/defaultState.js
+++ src/cerebral/defaultState.js
@@ -13,6 +13,22 @@ export default (s) => ({
                 offset: 0,
             },
         },
-        filters: [],
+        filters: [
+            {
+                id: 1,
+                text: 'Добавлен собственник',
+                value: 'AddNewOwner'
+            },
+            {
+                id: 2,
+                text: 'Был импорт реестра',
+                value: 'Import'
+            },
+            {
+                id: 3,
+                text: 'Удален собственник',
+                value: 'DeleteOwner'
+            }
+        ],
     },
 });
--- src/cerebral/sequences.js
+++ src/cerebral/sequences.js
@@ -1,6 +1,11 @@
-import { set } from 'cerebral/factories';
+import { set, concat } from 'cerebral/factories';
 import { state, props } from 'cerebral';
 import * as actions from './actions';
+import {toggleCard} from "./actions";
+import {toggleFilter} from "./actions";
+import {changeFilter} from "./actions";
+import {addToHistory} from "./actions";
+import {setHistory} from "./actions";
 
 const loadPage = [
     actions.setRoute,
@@ -11,31 +16,36 @@ const loadPage = [
     set(state`isLoadingPage`, false)
 ];
 
-const changePage = [
-    set(state`isLoadingPaging`, true),
-    actions.getHistory,
-    actions.addHistory,
-    set(state`isLoadingPaging`, false),
-];
-
 export const routeToRoot = set(state`current.page`, 'root');
 
 export const routeToRegistry = [
     // actions.setBrowserUrl,
     set(state`current.page`, 'registry'),
-    set(state`isLoadingPage`, true),
+    set(state`history.isLoadingPage`, true),
     actions.clearItems,
     actions.getHistory,
     actions.setHistory,
-    set(state`isLoadingPage`, false)
+    set(state`history.isLoadingPage`, false)
 ];
 
 export const routeToNotFound = set(state`current.page`, 'notfound');
+
+export const changePage = [
+    set(state`isLoadingPaging`, true),
+    actions.nextMeta,
+    actions.getHistory,
+    actions.addToHistory,
+    set(state`isLoadingPaging`, false),
+];
 
 export default {
     loadPage,
     changePage,
     routeToRoot,
     routeToRegistry,
-    routeToNotFound
+    routeToNotFound,
+    toggleCard,
+    toggleFilter,
+    changeFilter,
+    setHistory,
 };
