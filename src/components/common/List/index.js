0f2b7dbb5be28d996c792a0bda83ad82ad9c1024
a.espolov <a.espolov@gmail.com>
2019-03-13 18:21:57 +0300

    ready last +1

--- src/components/common/List/index.js
+++ src/components/common/List/index.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import uniqueId from 'lodash/uniqueId';
 
-const List = ({
-  containerClass,
-  CardComponent,
-  isLoadingPage,
-  cellClass,
-  list
-}) => {
-    return isLoadingPage ? (
-            <CircularProgress variant="indeterminate" />
-        ) : (
-            list.map(item => (
-                <CardComponent
-                    key={`history-card-${uniqueId()}`}
-                    data={item}
-                />
-            ))
-        )
+const List = ({ CardComponent, isLoadingPage, list }) => {
+  return isLoadingPage ? (
+    <CircularProgress variant="indeterminate" />
+  ) : (
+    list.map(item => <CardComponent key={`history-card-${uniqueId()}`} data={item} />)
+  );
 };
 
 List.propTypes = {
-  containerClass: PropTypes.string.isRequired,
   CardComponent: PropTypes.func.isRequired,
   isLoadingPage: PropTypes.bool.isRequired,
-  cellClass: PropTypes.string.isRequired,
   list: PropTypes.arrayOf(PropTypes.shape({})).isRequired
-};
-
-List.defaultProps = {
-  headerClass: '',
-  contentClass: ''
 };
 
 export default List;
--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -3,9 +3,9 @@ import CardContent from '@material-ui/core/CardContent';
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as Page from 'page';
-import {connect} from '@cerebral/react';
-import {sequences, state} from 'cerebral';
-import {withStyles} from '@material-ui/core/styles';
+import { connect } from '@cerebral/react';
+import { sequences, state } from 'cerebral';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Filter from '../common/Filter';
 import Header from '../common/Header';
@@ -14,123 +14,105 @@ import List from '../common/List';
 
 // eslint-disable-next-line no-unused-vars
 const styles = theme => ({
-    content: {
-        flexGrow: 1,
-        width: 'auto'
-    },
-    card: {
-        margin: '0 auto',
-        boxShadow: 'none'
-    },
-    filtersAndList: {
-        padding: '0 !important'
-    },
-    header: {
-        borderBottom: '1px solid gray'
-    },
-    filtersClass: {
-        borderBottom: '1px solid gray'
-    },
-    cardClass: {
-        borderBottom: '1px solid gray'
-    },
-    cell: {
-        width: '100%',
-        textAlign: 'center'
-    },
-    paging: {
-        marginTop: 20,
-        marginBottom: 50
-    },
-    items: {
-        marginTop: 20,
-        width: '100%',
-    }
+  content: {
+    flexGrow: 1,
+    width: 'auto'
+  },
+  card: {
+    margin: '0 auto',
+    boxShadow: 'none'
+  },
+  filtersAndList: {
+    padding: '0 !important'
+  },
+  header: {
+    borderBottom: '1px solid gray'
+  },
+  filtersClass: {
+    borderBottom: '1px solid gray'
+  },
+  cardClass: {
+    borderBottom: '1px solid gray'
+  },
+  cell: {
+    width: '100%',
+    textAlign: 'center'
+  },
+  paging: {
+    marginTop: 20,
+    marginBottom: 50
+  },
+  items: {
+    marginTop: 20,
+    width: '100%'
+  }
 });
 
 const History = props => {
-    const {get, routes, classes} = props;
-    const page = get(state`current.page`);
-    const toggleFilter = get(sequences`toggleFilter`);
-    const changeFilter = get(sequences`changeFilter`);
-    const changePage = get(sequences`changePage`);
-    const {
-        isLoadingPage,
-        isLoadingPaging,
-        isShowFilter,
-        items: {list},
-        filters,
-        error
-    } = get(state`history`);
+  const { get, routes, classes } = props;
+  const page = get(state`current.page`);
+  const toggleFilter = get(sequences`toggleFilter`);
+  const changeFilter = get(sequences`changeFilter`);
+  const changePage = get(sequences`changePage`);
+  const {
+    isLoadingPage,
+    isLoadingPaging,
+    isShowFilter,
+    items: { list },
+    filters,
+    error
+  } = get(state`history`);
 
-    let resultView;
-    if (error) {
-        resultView = (
-            <p>
-                <code>{error}</code>
-            </p>
-        );
-    }
-
-    if (resultView) {
-        return resultView;
-    }
-
-    const filterView = isShowFilter && (
-        <Filter filtersClass={classes.filtersClass} filters={filters} changeFilter={changeFilter}/>
+  let resultView;
+  if (error) {
+    resultView = (
+      <p>
+        <code>{error}</code>
+      </p>
     );
+  }
 
-    const CardComponent = routes[page].componentChildren;
+  if (resultView) {
+    return resultView;
+  }
 
-    console.log('classes.items', classes.items);
-    console.log('classes.cell', classes.cell);
-    console.log('classes.cardClass', classes.cardClass);
-    console.log('CardComponent', CardComponent);
-    console.log('classes.contentCard', classes.contentCard);
-    console.log('classes.headerCard', classes.headerCard);
-    console.log('isLoadingPage', isLoadingPage);
-    console.log('list', list);
+  const filterView = isShowFilter && (
+    <Filter filtersClass={classes.filtersClass} filters={filters} changeFilter={changeFilter} />
+  );
+  const CardComponent = routes[page].componentChildren;
 
-
-    return (
-        <div className={classes.content}>
-            <Grid container spacing={24}>
-                <Grid container xs={12}>
-                    <Card className={classes.card}>
-                        <Header
-                            title={routes[page].title}
-                            toggleFilter={toggleFilter}
-                            toIndex={() => Page.show('/')}
-                            className={classes.header}
-                        />
-                        <CardContent className={classes.filtersAndList}>
-                            {filterView}
-                            <List
-                                containerClass={classes.items}
-                                cellClass={classes.cell}
-                                cardClass={classes.cardClass}
-                                CardComponent={CardComponent}
-                                isLoadingPage={isLoadingPage}
-                                list={list}
-                            />
-                            <Paging
-                                containerClass={classes.paging}
-                                cellClass={classes.cell}
-                                isLoadingPaging={isLoadingPaging}
-                                changePage={changePage}
-                            />
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div className={classes.content}>
+      <Grid container spacing={24}>
+        <Grid container xs={12}>
+          <Card className={classes.card}>
+            <Header
+              title={routes[page].title}
+              toggleFilter={toggleFilter}
+              toIndex={() => Page.show('/')}
+              className={classes.header}
+            />
+            <CardContent className={classes.filtersAndList}>
+              {filterView}
+              <List CardComponent={CardComponent} isLoadingPage={isLoadingPage} list={list} />
+              <Paging
+                containerClass={classes.paging}
+                cellClass={classes.cell}
+                isLoadingPaging={isLoadingPaging}
+                changePage={changePage}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 History.propTypes = {
-    routes: PropTypes.shape({}).isRequired,
-    get: PropTypes.func.isRequired,
-    classes: PropTypes.shape({}).isRequired
+  routes: PropTypes.shape({}).isRequired,
+  get: PropTypes.func.isRequired,
+  classes: PropTypes.shape({}).isRequired
 };
 
 export default withStyles(styles)(connect(History));
