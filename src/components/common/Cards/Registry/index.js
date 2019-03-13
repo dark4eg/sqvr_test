409c66e6ca96be02745343c7c0144637a67cad2e
a.espolov <a.espolov@gmail.com>
2019-03-13 04:42:22 +0300

    ready lint

--- src/components/common/Cards/Registry/index.js
+++ src/components/common/Cards/Registry/index.js
@@ -1,4 +1,6 @@
+/* eslint react/no-danger: off */
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -18,17 +20,20 @@ class Registry extends Component {
   }
 
   toggleAdditional = () => {
-    this.setState({ additionalIsActive: !this.state.additionalIsActive });
+    const { additionalIsActive } = this.state;
+    this.setState({ additionalIsActive: !additionalIsActive });
   };
 
   render() {
-    const { date, user, updater, systemText } = this.props.data;
+    const { data } = this.props;
+    const { date, user, updater, systemText } = data;
     const { additionalIsActive } = this.state;
     const expandBtnView = additionalIsActive ? <ExpandLess /> : <ExpandMore />;
 
-    let avatarView;
+    let avatarUserView;
+    let avatarUpdaterView;
     if (user.avatarUrl) {
-      avatarView = (
+      avatarUserView = (
         <a href={`/history/user/${user.id}/`}>
           <Avatar
             sizes="20,20"
@@ -39,25 +44,39 @@ class Registry extends Component {
         </a>
       );
     } else if (user.firstName && user.lastName) {
-      avatarView = (
-        <Avatar className="" alt={`${user.firstName} ${user.lastName}`}>
-          {`${user.firstName[0]} ${user.lastName[0]}`}
-        </Avatar>
+      avatarUserView = (
+        <a href={`/history/user/${user.id}/`}>
+          <Avatar className="" alt={`${user.firstName} ${user.lastName}`}>
+            {`${user.firstName[0]} ${user.lastName[0]}`}
+          </Avatar>
+        </a>
+      );
+    }
+    if (updater.avatarUrl) {
+      avatarUpdaterView = (
+        <a href={`/history/user/${updater.id}/`}>
+          <Avatar
+            sizes="20,20"
+            className=""
+            alt={`${updater.firstName} ${updater.lastName}`}
+            src={updater.avatarUrl}
+          />
+        </a>
+      );
+    } else if (updater.firstName && updater.lastName) {
+      avatarUpdaterView = (
+        <a href={`/history/user/${updater.id}/`}>
+          <Avatar className="" alt={`${updater.firstName} ${updater.lastName}`}>
+            {`${updater.firstName[0]} ${updater.lastName[0]}`}
+          </Avatar>
+        </a>
       );
     }
 
     return (
       <Card className="">
         <CardHeader
-          avatar={
-            <a href={`/history/user/${updater.id}/`}>
-  <Avatar
-                className=""
-                alt={`${updater.firstName} ${updater.lastName}`}
-                src={updater.avatarUrl}
-              />
-</a>
-          }
+          avatar={avatarUpdaterView}
           title={`${updater.firstName} ${updater.lastName}`}
           subheader={convertDate(date)}
           action={<IconButton onClick={this.toggleAdditional}>{expandBtnView}</IconButton>}
@@ -66,7 +85,7 @@ class Registry extends Component {
           <p dangerouslySetInnerHTML={{ __html: `${systemText},` }} />
           <div>
             <span>собственник </span>
-            {avatarView}
+            {avatarUserView}
             <span>{`${user.firstName} ${user.lastName}`}</span>
           </div>
           {additionalIsActive && (
@@ -80,5 +99,14 @@ class Registry extends Component {
     );
   }
 }
+
+Registry.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    user: PropTypes.shape({}).isRequired,
+    updater: PropTypes.shape({}).isRequired,
+    systemText: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default Registry;
--- src/components/common/Cards/User/index.js
+++ src/components/common/Cards/User/index.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
+/* eslint react/no-danger: off */
+import React from 'react';
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import { convertDate } from '../../../../utils/date';
 
-class User extends Component {
-  render() {
-    const { date, updater, systemText } = this.props.data;
+const User = ({ data }) => {
+  const { date, updater, systemText } = data;
 
-    return (
-      <Card className="">
-        <CardHeader
-          avatar={
-            <Avatar
-  className=""
-  alt={`${updater.firstName} ${updater.lastName}`}
-  src={updater.avatarUrl}
-/>
-          }
-          title={`${updater.firstName} ${updater.lastName}`}
-          subheader={convertDate(date)}
-        />
-        <CardContent>
-          <p dangerouslySetInnerHTML={{ __html: `${systemText},` }} />
-        </CardContent>
-      </Card>
-    );
-  }
-}
+  const avatarView = (
+    <Avatar className="" alt={`${updater.firstName} ${updater.lastName}`} src={updater.avatarUrl} />
+  );
+
+  return (
+    <Card className="">
+      <CardHeader
+        avatar={avatarView}
+        title={`${updater.firstName} ${updater.lastName}`}
+        subheader={convertDate(date)}
+      />
+      <CardContent>
+        <p dangerouslySetInnerHTML={{ __html: `${systemText},` }} />
+      </CardContent>
+    </Card>
+  );
+};
+
+User.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    // user: PropTypes.shape({}).isRequired,
+    updater: PropTypes.shape({}).isRequired,
+    systemText: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default User;
